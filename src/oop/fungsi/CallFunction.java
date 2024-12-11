e64acc80eb38eaefa7e5f53966695836e32389e3
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-12-11 10:26:29 +0700

    function

---
+++ src/oop/fungsi/CallFunction.java
@@ -0,0 +1,12 @@
+package oop.fungsi;
+
+public class CallFunction {
+    public static void main(String[] args) {
+        // memanggil fungsi
+        cobaFungsi();
+    }
+
+    public static void cobaFungsi() {
+        System.out.println("Ini merupakan bagian dari fungsi");
+    }
+}
