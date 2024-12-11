ee504e61947840d38124b9e12ad163f5b9cc9d30
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-12-11 13:58:17 +0700

    access modifier private

---
+++ src/accessmodifier/privates/KelasA.java
@@ -0,0 +1,15 @@
+package accessmodifier.privates;
+
+public class KelasA {
+    private int memberA = 5;
+
+    private int functionA() {
+        return memberA;
+    }
+
+    int functionB() {
+        // Pemanggilan private member dan private function
+        int hasil = functionA() + memberA;
+        return hasil;
+    }
+}
---
+++ src/accessmodifier/privates/Main.java
@@ -0,0 +1,8 @@
+package accessmodifier.privates;
+
+public class Main {
+    public static void main(String[] args) {
+        KelasA kelasA = new KelasA();
+        System.out.println(kelasA.functionB());
+    }
+}
