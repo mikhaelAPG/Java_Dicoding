b2ee2a2c9c7d29a2c4090a5b45f3dc6563e72114
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-12-11 10:29:33 +0700

    properti

---
+++ src/propertimetode/Hewan.java
@@ -0,0 +1,17 @@
+package propertimetode;
+
+public class Hewan {
+    // Properti atau fields
+
+    // Inisiasi dengan nilai inisial
+    double tinggi = 30;
+    double berat = 3;
+
+    // Inisiasi melalui konstruktor
+    int umur;
+
+    // Konstruktor dengan parameter
+    Hewan(int umur) {
+        this.umur = umur;
+    }
+}
