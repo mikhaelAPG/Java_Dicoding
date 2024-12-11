cb8ba5af05049bf021592fb4a84e27151e650d38
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-12-11 15:03:50 +0700

    Input and Output Stream

---
+++ src/IOStreams/Main.java
@@ -0,0 +1,78 @@
+package IOStreams;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+    //            FileInputStream in = null;
+    //            FileOutputStream out = null;
+    //
+    //            try {
+    //                in = new FileInputStream("latihan_input.txt");
+    //                out = new FileOutputStream("latihan_ouput.txt");
+    //                int c;
+    //
+    //                while ((c = in.read()) != -1) {
+    //                    out.write(c);
+    //                }
+    //            } catch (IOException e) {
+    //                e.printStackTrace();
+    //            } finally {
+    //                try {
+    //                    if (in != null) {
+    //                        in.close();
+    //                    }
+    //                    if (out != null) {
+    //                        out.close();
+    //                    }
+    //                } catch (IOException e) {
+    //                    e.printStackTrace();
+    //                }
+    //            }
+
+    //        FileReader in = null;
+    //        FileWriter out = null;
+    //        try {
+    //            in = new FileReader("latihan_input.txt");
+    //            out = new FileWriter("latihan_ouput.txt");
+    //            int c;
+    //            while ((c = in.read()) != -1) {
+    //                out.write(c);
+    //            }
+    //        } catch (IOException e) {
+    //            e.printStackTrace();
+    //        } finally {
+    //            try {
+    //                if (in != null) {
+    //                    in.close();
+    //                }
+    //                if (out != null) {
+    //                    out.close();
+    //                }
+    //            } catch (IOException e) {
+    //                e.printStackTrace();
+    //            }
+    //        }
+
+    //        String dirname = "/java/latihan1";
+    //        File file = new File(dirname);
+    //        // Buat directory
+    //        file.mkdirs();
+
+        String dirname = "/java/latihan1";
+        File file = null;
+        String[] paths;
+        try {
+            // Instansiasi objek File
+            file = new File(dirname);
+            // Ambil list files dan masukkan ke string paths
+            paths = file.list();
+            // Tampilkan semua path yang ada
+            for (String path : paths) {
+                System.out.println(path);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
