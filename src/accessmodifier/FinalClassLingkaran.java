7d6e38fc4c36a3f114fe716d35939531794a5342
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-12-11 14:21:32 +0700

    final, synchronized, trancient

---
+++ src/accessmodifier/FinalClassLingkaran.java
@@ -0,0 +1,4 @@
+package accessmodifier;
+
+final class FinalClassLingkaran {
+}
--- src/accessmodifier/protect/package3/Perhitungan.java
+++ src/accessmodifier/protect/package3/Perhitungan.java
@@ -1,6 +1,16 @@
 package accessmodifier.protect.package3;
 
 public class Perhitungan {
+        /*
+        * Final
+        * */
+        //    final int nilai = 5;
+        //
+        //    void ubahNilai(){
+        //        // Kode di bawah akan menampilkan error
+        //        nilai = 10;
+        //    }
+
     public static int nilai = 0;
 
     protected static int getNilai(){
