44c6114790423067f82d8aaaff7397d93e2ae3c7
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-12-12 07:09:25 +0700

    datetime

--- src/IOStreams/Main.java
+++ src/IOStreams/Main.java
@@ -4,14 +4,38 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-    //            FileInputStream in = null;
-    //            FileOutputStream out = null;
+    //                FileInputStream in = null;
+    //                FileOutputStream out = null;
     //
+    //                try {
+    //                    in = new FileInputStream("C:\\Users\\USER\\Documents\\Belajar\\Java Dicoding\\java_dicoding\\src\\IOStreams\\latihan_input.txt");
+    //                    out = new FileOutputStream("latihan_output.txt");
+    //                    int c;
+    //
+    //                    while ((c = in.read()) != -1) {
+    //                        out.write(c);
+    //                    }
+    //                } catch (IOException e) {
+    //                    e.printStackTrace();
+    //                } finally {
+    //                    try {
+    //                        if (in != null) {
+    //                            in.close();
+    //                        }
+    //                        if (out != null) {
+    //                            out.close();
+    //                        }
+    //                    } catch (IOException e) {
+    //                        e.printStackTrace();
+    //                    }
+    //                }
+
+    //            FileReader in = null;
+    //            FileWriter out = null;
     //            try {
-    //                in = new FileInputStream("latihan_input.txt");
-    //                out = new FileOutputStream("latihan_ouput.txt");
+    //                in = new FileReader("C:\\Users\\USER\\Documents\\Belajar\\Java Dicoding\\java_dicoding\\src\\IOStreams\\latihan_input.txt");
+    //                out = new FileWriter("latihan_ouput.txt");
     //                int c;
-    //
     //                while ((c = in.read()) != -1) {
     //                    out.write(c);
     //                }
@@ -29,50 +53,26 @@ public class Main {
     //                    e.printStackTrace();
     //                }
     //            }
-
-    //        FileReader in = null;
-    //        FileWriter out = null;
-    //        try {
-    //            in = new FileReader("latihan_input.txt");
-    //            out = new FileWriter("latihan_ouput.txt");
-    //            int c;
-    //            while ((c = in.read()) != -1) {
-    //                out.write(c);
-    //            }
-    //        } catch (IOException e) {
-    //            e.printStackTrace();
-    //        } finally {
-    //            try {
-    //                if (in != null) {
-    //                    in.close();
-    //                }
-    //                if (out != null) {
-    //                    out.close();
-    //                }
-    //            } catch (IOException e) {
-    //                e.printStackTrace();
-    //            }
-    //        }
+    //
+    //            String dirname = "/java/latihan1";
+    //            File file = new File(dirname);
+    //            // Buat directory
+    //            file.mkdirs();
 
     //        String dirname = "/java/latihan1";
-    //        File file = new File(dirname);
-    //        // Buat directory
-    //        file.mkdirs();
-
-        String dirname = "/java/latihan1";
-        File file = null;
-        String[] paths;
-        try {
-            // Instansiasi objek File
-            file = new File(dirname);
-            // Ambil list files dan masukkan ke string paths
-            paths = file.list();
-            // Tampilkan semua path yang ada
-            for (String path : paths) {
-                System.out.println(path);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    //        File file = null;
+    //        String[] paths;
+    //        try {
+    //            // Instansiasi objek File
+    //            file = new File(dirname);
+    //            // Ambil list files dan masukkan ke string paths
+    //            paths = file.list();
+    //            // Tampilkan semua path yang ada
+    //            for (String path : paths) {
+    //                System.out.println(path);
+    //            }
+    //        } catch (Exception e) {
+    //            e.printStackTrace();
+    //        }
     }
 }
